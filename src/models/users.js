3cd2482ffef2cd28704f18b3bcfb6bfbe31536c5
haein <kendy93@sbcn.co.kr>
2019-02-13 15:28:50 +0900

    related #2 users 라우터 작업 및 스키마 생성

---
+++ src/models/users.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const Users = new Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, trim: true },
+    user_name: String,
+    push: {
+      type: Boolean,
+      default: true
+    },
+    user_thumnail: {
+      type: String,
+      default: "user"
+    },
+    created_at: {
+      type: Date,
+      default: new Date()
+    },
+    updated_at: {
+      type: Date,
+      default: null
+    },
+    end: {
+      type: Boolean,
+      default: false
+    }
+  }
+  // {
+  //   versionKey: false // You should be aware of the outcome after set to false
+  // }
+);
+
+module.exports = mongoose.model("Users", Users);
