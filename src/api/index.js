bd4298e4eef13d7d4e7a3b4556667987efd157da
haein <kendy93@sbcn.co.kr>
2019-02-13 11:15:30 +0900

    colsed #1 test API 작업

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 const Router = require("koa-router");
 const posts = require("./posts");
-const users = require("./users");
+// const users = require("./users");
 // const habit = require("./habit");
 // const rewords = require("./rewords");
 
@@ -9,7 +9,7 @@ const api = new Router();
 //test
 api.use("/posts", posts.routes());
 //사용자
-api.use("/users", users.routes());
+// api.use("/users", users.routes());
 //습관
 // api.use("/habit", habit.routes());
 // //보상
