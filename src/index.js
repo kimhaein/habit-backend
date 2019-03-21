4373cc091b4f0cdf8785e69cbe8428f92db018a0
haein <kendy93@sbcn.co.kr>
2019-03-22 08:51:54 +0900

    heroku 셋팅중

--- src/index.js
+++ src/index.js
@@ -31,9 +31,10 @@ mongoose.set("useCreateIndex", true);
 
 // 라우터 설정
 router.use("/api", api.routes()); // api 라우트 적용
-router.use("/", function() {
-  console.log(111);
-}); // api 라우트 적용
+
+router.get("/", async ctx => {
+  ctx.body = "Hello World!";
+});
 
 // 라우터 적용 전에 bodyParser 적용
 app.use(bodyParser());
@@ -49,7 +50,7 @@ app.keys = [signKey];
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(process.env.PORT || port, () => {
+app.listen(port, () => {
   console.log("listening to port", port);
 });
 
